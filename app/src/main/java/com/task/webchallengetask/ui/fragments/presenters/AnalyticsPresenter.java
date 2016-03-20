e5a977ee09a35d4c3a01a8fa16108c060ba1b935
Klim <Kamil@i.ua>
2016-03-20 13:34:28 +0200

    base architecture + MVP

--- app/src/main/java/com/task/webchallengetask/ui/fragments/presenters/AnalyticsPresenter.java
+++ app/src/main/java/com/task/webchallengetask/ui/fragments/presenters/AnalyticsPresenter.java
@@ -1,9 +1,18 @@
 package com.task.webchallengetask.ui.fragments.presenters;
 
+import com.task.webchallengetask.data.data_providers.AnalitycsDataProvider;
 import com.task.webchallengetask.ui.base.BaseFragmentPresenter;
 import com.task.webchallengetask.ui.base.BaseView;
 
 public class AnalyticsPresenter extends BaseFragmentPresenter<AnalyticsPresenter.AnalyticsView> {
+
+    private AnalitycsDataProvider dataProvider = AnalitycsDataProvider.getInstance();
+
+    @Override
+    public void onViewCreated() {
+        super.onViewCreated();
+        dataProvider.getSomeData().subscribe(strings -> {});
+    }
 
     public interface AnalyticsView extends BaseView {
 
