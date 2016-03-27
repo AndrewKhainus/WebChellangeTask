84f1387326843a649294f6583c3604929748b515
Sergbek <sergit.gevelev@thinlmobiles.com>
2016-03-27 15:05:39 +0300

    edit listActivity

--- app/src/main/java/com/task/webchallengetask/ui/modules/activity/presenters/ActivityListPresenter.java
+++ app/src/main/java/com/task/webchallengetask/ui/modules/activity/presenters/ActivityListPresenter.java
@@ -6,28 +6,22 @@ import com.task.webchallengetask.data.data_providers.ActivityDataProvider;
 import com.task.webchallengetask.data.database.tables.ActionParametersModel;
 import com.task.webchallengetask.global.Constants;
 import com.task.webchallengetask.global.utils.TimeUtil;
-import com.task.webchallengetask.ui.modules.activity.views.ActivityStartActivity;
-import com.task.webchallengetask.ui.modules.activity.views.ActivityDetailFragment;
 import com.task.webchallengetask.ui.base.BaseFragmentPresenter;
 import com.task.webchallengetask.ui.base.BaseFragmentView;
+import com.task.webchallengetask.ui.modules.activity.views.ActivityDetailFragment;
+import com.task.webchallengetask.ui.modules.activity.views.ActivityStartActivity;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 public class ActivityListPresenter extends BaseFragmentPresenter<ActivityListPresenter.ActivityListView> {
 
-    private List<ActionParametersModel> actionParametersModels;
-
     @Override
     public void onViewCreated() {
         super.onViewCreated();
-        actionParametersModels= new ArrayList<>();
-
         ActivityDataProvider.getInstance()
                 .getActivities()
-                .doOnNext(this::sortList)
+                .doOnNext(this::fillList)
                 .exists(t ->t != null && t.size() == 0)
                 .doOnNext(_boolean -> {
                     if (_boolean) getView().showHolder();
@@ -37,8 +31,8 @@ public class ActivityListPresenter extends BaseFragmentPresenter<ActivityListPre
 
     }
 
-    private void fillList(List<ActionParametersModel> _modelList){
-         if (!_modelList.isEmpty()) {
+    private void fillList(List<ActionParametersModel> _modelList) {
+        if (!_modelList.isEmpty()) {
 
             long previousDay = _modelList.get(0).getStartTime();
             ActionParametersModel previousModel = _modelList.get(0);
@@ -47,16 +41,17 @@ public class ActivityListPresenter extends BaseFragmentPresenter<ActivityListPre
 
             for (int i = 1; i < _modelList.size(); i++) {
                 long currentDay = _modelList.get(i).getStartTime();
-                if (TimeUtil.compareDay(previousDay, currentDay) != 0 ) {
+                if (TimeUtil.compareDay(previousDay, currentDay) != 0) {
                     getView().addActivities(currentList);
                     currentList.clear();
-                }  else {
+                } else {
                     currentList.add(_modelList.get(i));
                 }
                 previousDay = currentDay;
             }
 
             getView().addActivities(currentList);
+        }
     }
 
     public void onFABClicked() {
